68e8ad8ce7615426a91b8282ec97831186fd9898
Martin Thoma <info@martin-thoma.de>
2020-11-15 11:03:45 +0100

    Send drawn image and store it

--- minimal_symbol_recognizer/app.py
+++ minimal_symbol_recognizer/app.py
@@ -1,10 +1,12 @@
 """Run a Flask web server for symbol recognition."""
 
 # Core Library modules
+import base64
 from pathlib import Path
+from typing import Any, Dict
 
 # Third party modules
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 model = None
 
@@ -20,6 +22,14 @@ def create_app() -> Flask:
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/classify", methods=["POST"])
+    def classify() -> Dict[str, Any]:
+        imagestr = request.form["imgBase64"]
+        with open("example.png", "wb") as fp:
+            decoded = base64.b64decode(imagestr.split(",")[1])
+            fp.write(decoded)
+        return {"errors": []}
 
     return app
 
